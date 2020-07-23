f0387e21ce666e2e9122c6fac90936731f8dc6f7
Xiasong Chen <296724771@qq.com>
2020-07-23 23:25:52 +0100

    max tree

--- src/main/java/com/siazon/tree/Tree.java
+++ src/main/java/com/siazon/tree/Tree.java
@@ -487,5 +487,32 @@ public class Tree {
                 postorder, postBegin + lenOfLeft, postEnd - 1);
         return root;
     }
+
+    //654.最大二叉树
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return constructMaximumBinaryTree1(nums, 0, nums.length);
+    }
+
+    public TreeNode constructMaximumBinaryTree1(int[] nums, int leftIndex, int rightIndex) {
+        if (rightIndex - leftIndex < 1) {// 没有元素了
+            return null;
+        }
+        if (rightIndex - leftIndex == 1) {// 只有一个元素
+            return new TreeNode(nums[leftIndex]);
+        }
+        int maxIndex = leftIndex;// 最大值所在位置
+        int maxVal = nums[maxIndex];// 最大值
+        for (int i = leftIndex + 1; i < rightIndex; i++) {
+            if (nums[i] > maxVal) {
+                maxVal = nums[i];
+                maxIndex = i;
+            }
+        }
+        TreeNode root = new TreeNode(maxVal);
+        // 根据maxIndex划分左右子树
+        root.left = constructMaximumBinaryTree1(nums, leftIndex, maxIndex);
+        root.right = constructMaximumBinaryTree1(nums, maxIndex + 1, rightIndex);
+        return root;
+    }
 }
 
