b84809afaee0aa062e83465f2038b7d288840a0f
Xiasong Chen <296724771@qq.com>
2022-10-24 22:48:41 +0100

    'symmetricl'

--- src/main/java/com/siazon/tree/Tree.java
+++ src/main/java/com/siazon/tree/Tree.java
@@ -122,7 +122,6 @@ public class Tree {
                 nextNodes.add(node.left);
             if (node.right != null)
                 nextNodes.add(node.right);
-
         }
         result.add(nodes);
         levelTraversalSub(nextNodes);
@@ -224,6 +223,32 @@ public class Tree {
         inorder(root.right, list);
         list.add(root.val);
 
+    }
+
+    //对称二叉树
+    public boolean isSymmetric1(TreeNode root) {
+        return compare(root.left, root.right);
+    }
+
+    private boolean compare(TreeNode left, TreeNode right) {
+        if (left == null && right != null) {
+            return false;
+        }
+        if (left != null && right == null) {
+            return false;
+        }
+
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        // 比较外侧
+        boolean compareOutside = compare(left.left, right.right);
+        // 比较内侧
+        boolean compareInside = compare(left.right, right.left);
+        return compareOutside && compareInside;
     }
 }
 
