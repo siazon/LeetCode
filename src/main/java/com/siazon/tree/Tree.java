941abbb6054a72b0769419a140a65b6f7c2c3645
Xiasong Chen <296724771@qq.com>
2022-11-14 23:15:23 +0000

    merge tree

--- src/main/java/com/siazon/tree/Tree.java
+++ src/main/java/com/siazon/tree/Tree.java
@@ -514,5 +514,14 @@ public class Tree {
         root.right = constructMaximumBinaryTree1(nums, maxIndex + 1, rightIndex);
         return root;
     }
+
+    public TreeNode mergeTree(TreeNode root1, TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+        root1.val += root2.val;
+        root1.left = mergeTree(root1.left, root2.left);
+        root1.right = mergeTree(root1.right, root2.right);
+        return root1;
+    }
 }
 
