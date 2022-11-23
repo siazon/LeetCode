85b2d4dbad9e5f290b8c99078bc5d60f2b346cd3
Xiasong Chen <296724771@qq.com>
2022-11-23 23:44:21 +0000

    deletenode

--- src/main/java/com/siazon/tree/Tree.java
+++ src/main/java/com/siazon/tree/Tree.java
@@ -668,7 +668,21 @@ public class Tree {
     //450.删除二叉搜索树中的节点
     public TreeNode deleteNode(TreeNode root, int val) {
         if (root == null) return null;
-        if (root.val < val)
+        if (root.val < val) {
+            root.right = deleteNode(root.right, val);
+        } else if (root.val > val) {
+            root.left = deleteNode(root.left, val);
+        } else {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            TreeNode temp = root.right;
+            while (temp.left != null) {
+                temp = temp.left;
+            }
+            root.val = temp.val;
+            root.right = deleteNode(root.right, val);
+        }
+        return root;
     }
 }
 
