dc94f4541ae1ea4646bccfa68d539024cb3ae874
Xiasong Chen <296724771@qq.com>
2022-12-01 23:47:41 +0000

    wiggle

--- src/main/java/com/siazon/greed/Greed.java
+++ src/main/java/com/siazon/greed/Greed.java
@@ -16,4 +16,19 @@ public class Greed {
         }
         return count;
     }
+
+    //376. 摆动序列
+    public int wiggleMaxLength(int[] nums) {
+        if (nums.length <= 1) return nums.length;
+        int pre = 0, curr = 0;
+        int res = 1;
+        for (int i = 1; i < nums.length; i++) {
+            curr = nums[i] - nums[i - 1];
+            if ((curr > 0 && pre <= 0) || (curr <= 0 && pre > 0)) {
+                res++;
+                pre = curr;
+            }
+        }
+        return res;
+    }
 }
