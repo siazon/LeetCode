6ad2ad5b3d3354b645d8ca1bde7c2333b0091082
Xiasong Chen <296724771@qq.com>
2022-12-02 23:22:30 +0000

    test

---
+++ src/test/java/com/siazon/greed/GreedTest.java
@@ -0,0 +1,25 @@
+package com.siazon.greed;
+
+import org.junit.jupiter.api.Test;
+
+class GreedTest {
+    Greed test = new Greed();
+
+    @Test
+    void getCookes() {
+    }
+
+    @Test
+    void wiggleMaxLength() {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int res = test.wiggleMaxLength(nums);
+        System.out.println(res);
+    }
+
+    @Test
+    void wiggleMaxLengthDP() {
+        int[] nums = new int[]{1, 2, 4, 3, 5, 6, 7, 8, 9};
+        int res = test.wiggleMaxLengthDP(nums);
+        System.out.println(res);
+    }
+}
