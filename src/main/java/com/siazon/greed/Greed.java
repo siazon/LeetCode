16f2ef718658f38447ee4ef1e03ccf9b45cf0b65
Xiasong Chen <296724771@qq.com>
2022-12-09 21:47:39 +0000

    jumpgame

--- src/main/java/com/siazon/greed/Greed.java
+++ src/main/java/com/siazon/greed/Greed.java
@@ -97,4 +97,16 @@ public class Greed {
         }
         return result;
     }
+
+    //55. 跳跃游戏
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) return true;
+        int cover = 0;
+        for (int i = 0; i <= cover; i++) {
+            cover = Math.max(cover, i + nums[i]);
+            if (cover >= nums.length - 1)
+                return true;
+        }
+        return false;
+    }
 }
