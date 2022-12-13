0269f9abadc60762153e58a6228dd59bd368d612
Xiasong Chen <296724771@qq.com>
2022-12-13 21:25:35 +0000

    lemonadeChange

--- src/main/java/com/siazon/greed/Greed.java
+++ src/main/java/com/siazon/greed/Greed.java
@@ -181,4 +181,31 @@ public class Greed {
         return res;
 
     }
+
+    //860.柠檬水找零
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0, twenty = 0;
+        for (int i : bills) {
+            if (i == 5)
+                five++;
+            else if (i == 10) {
+                if (five <= 0) return false;
+                five--;
+                ten++;
+            } else if (i == 20) {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                    twenty++;
+                } else if (five >= 3) {
+                    five -= 3;
+                    twenty++;
+                } else {
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
 }
